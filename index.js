1e24a166974fbf049b3ef78fde7c76222840e082
Lawzennn <37016897+Lawzennn@users.noreply.github.com>
2018-03-04 03:34:50 +0100

    Update index.js

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ var bot = new Discord.Client();
 var prefix = ("/");
 
   bot.on('ready', () => {
-    bot.user.setPresence({ game: { name: '/help ByLawzenn ©', type: 0} });
+    bot.user.setPresence({ game: { name: 'LawBot (BETA)', type: 0} });
     console.log("Je suis connecté !");
 });
 
