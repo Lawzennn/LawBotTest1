a8bf2e4df489e90796b482b15bf114a3616ca692
Lawzennn <37016897+Lawzennn@users.noreply.github.com>
2018-03-04 02:27:41 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
 const Discord = require("discord.js");
 const client = new Discord.Client();
 
-bot.login(process.env.BOT_TOKEN);
-
+const token = "MjUwMDA1MDI3MTUyNzIzOTY5.CxOi3g.T12XrjS8QW6qjTG6qwkIODRUNhI";
 var prefix = ".";
 var mention = "<@1930903359700619264>";
 var memberCount = client.users.size;
@@ -11,7 +10,7 @@ var servercount = client.guilds.size;
 client.on("ready", () => {
 	var servers = client.guilds.array().map(g => g.name).join(',');
 	console.log("--------------------------------------");
-console.log('[!]Connexion en cours... \n[!]Veuillez Patienté! \n[!]Les évenement sont après ! :)  \n[!]Les préfix actuelle:  ' + prefix + "\n[!]Mentions = " + mention + "\n[!]Nombre de membres: " + memberCount + "\n[!]Nombre de serveurs: " + servercount);
+console.log('[!]Connexion en cours... \n[!]Veuillez Patienté! \n[!]Les évenement sont après ! :)  \n[!]Les préfix actuelle:  ' + prefix + "\n[!]Mentions = " + mention + "\n[!]Nombre de membres: " + memberCount + "\n[!]Nombre de serveurs: " + servercount);
 });
 
 client.on('message', message => {
@@ -30,3 +29,4 @@ client.on('message', message => {
 }
 });
 
+client.login(token)
